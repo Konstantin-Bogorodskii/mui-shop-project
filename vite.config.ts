e47eb383b5f46d9a8d54f2f26bf28b4370c01ceb
Konstantin Bogorodskii <konstantin.bogorodskii@gmail.com>
2024-04-30 15:49:27 +0300

    added path aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+import path from 'path';
+
 export default defineConfig({
-  plugins: [react()],
-})
+	plugins: [react()],
+	resolve: {
+		alias: {
+			'@': path.resolve(__dirname, './src'),
+			'@components': path.resolve(__dirname, './src/components'),
+			'@shared': path.resolve(__dirname, './src/components/shared'),
+			'@assets': path.resolve(__dirname, './src/assets')
+		}
+	}
+});
