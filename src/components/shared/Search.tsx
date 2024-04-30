2b4e4b3f05ad08c6e7c221fa52de8154c9adf248
Konstantin Bogorodskii <konstantin.bogorodskii@gmail.com>
2024-04-30 18:24:37 +0300

    changed HTML input element on TextField from mui

--- src/components/shared/Search.tsx
+++ src/components/shared/Search.tsx
@@ -1,3 +1,5 @@
+import { TextField } from '@mui/material';
+
 interface ISearchProps {
 	value: string;
 	onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
@@ -5,10 +7,16 @@ interface ISearchProps {
 
 const Search: React.FC<ISearchProps> = ({ onChange, value }) => {
 	return (
-		<input
+		<TextField
+			label="search"
+			variant="standard"
+			fullWidth
 			type="search"
 			value={value}
 			onChange={onChange}
+			sx={{
+				mb: '1.5rem'
+			}}
 		/>
 	);
 };
